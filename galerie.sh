4173a61e936b2d3417e603226c2b0a324f7353b3
Alexandre Pintrand <60129804+WharkInterface@users.noreply.github.com>
2020-12-04 13:37:33 +0100

    Add files via upload

--- galerie.sh
+++ galerie.sh
@@ -53,8 +53,8 @@ while :; do
            then
                convert -thumbnail "300x300" "$I" miniatures/"$I"
                echo "$I success."
+               echo "<a href='"$I"'><img src=\"miniatures/$I\" alt=""></a>" >> "miniatures.html"
            fi
-           echo "<a href='"$I"'><img src=\"miniatures/$I\" alt=""></a>" >> "miniatures.html"
            done
            echo "</body>
            </html>" >> "miniatures.html";;
