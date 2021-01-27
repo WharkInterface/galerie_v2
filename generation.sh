4e8e42e18fa500969dedc100dbbc14f1aa2f7745
Alexandre Pintrand <60129804+WharkInterface@users.noreply.github.com>
2021-01-27 10:14:26 +0100

    Add files via upload

--- generation.sh
+++ generation.sh
@@ -32,6 +32,8 @@ declare SEARCH="\[listeMiniatures\]"
                 convert -thumbnail "300x300" "$I" miniatures/"$I"
                 echo "$I success."
                 REPLACEMENT+="<div class=\"col-sm-6 col-md-4\"><div class=\"thumbnail\"><a class=\"lightbox\" href=\"$I\"><img src=\"miniatures/$I\" alt=""></a><div class=\"caption\"><h3>$I</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div></div></div>" # Concaténation du code HTML pour chaque image dans la variable REPLACEMENT
+            else # Sinon
+                echo "Aucun fichier avec l'extension $I n'a été trouvé dans le dossier miniatures." # On indique à l'utilisateur qu'il n'y a pas d'image en précisant les extensions.
             fi
         done
 
